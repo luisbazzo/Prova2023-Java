fcc9bc39f3633dd2dfc84fb7f09cd4ce7300b9e9
luisbazzo <luis.o.c.bazzo@gmail.com>
2023-06-20 08:09:30 -0300

    Commit Inicial

---
+++ src/main/java/com/eletiva/provabackend/ProvabackendApplication.java
@@ -0,0 +1,13 @@
+package com.eletiva.provabackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProvabackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProvabackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eletiva/provabackend/ProvabackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eletiva.provabackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProvabackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
